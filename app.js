40142108b8d1e5a37e5d2cd4e4b63281f7feeb6a
Dmitry K. Home <karasev.dmitry@gmail.com>
2020-03-17 23:06:14 +0400

    simple app

--- app.js
+++ app.js
@@ -1,37 +1,6 @@
 const http = require('http')
-const fs = require('fs')
+// const fs = require('fs')
+const router = require('./routes')
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.setHeader('Content-type', 'text/html')
-    res.write('<html>')
-    res.write('<header><title>My page</title></header>')
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">submit</button></form></body>')
-    res.write('</html>')
-    return res.end()
-  }
-  if (req.url === '/message' && req.method === "POST") {
-    const body = []
-    req.on('data', chunk => {
-      body.push(chunk)
-    })
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString()
-      const message = parsedBody.split('=')[1]
-      console.log(message)
-      fs.writeFile('message.txt', message, err => {
-        res.statusCode = 302
-        res.setHeader('Location', '/')
-        return res.end()
-      })
-    })
-    
-  }
-  res.setHeader('Content-type', 'text/html')
-  res.write('<html>')
-  res.write('<header><title>My page</title></header>')
-  res.write('<body><h1>Hello from my app!</h1></body>')
-  res.write('</html>')
-  res.end()
-})
+const server = http.createServer(router)
 server.listen(3000)
